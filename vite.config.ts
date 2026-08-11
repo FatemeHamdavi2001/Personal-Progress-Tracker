147a90a5bed5e9c9bb4a22bbb2f1520ea589f0a9
Fateme Hamdavi <Fateme.hamdavi2001@gmail.com>
2026-08-12 01:57:38 +0330

    Fix base path in vite.config and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/Personal-Progress-Tracker/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
